dfd825b0e3decf3b7e39adcd1ea388449a2606d7
kim_ingyeom <doctok501@gmail.com>
2023-08-07 16:07:54 +0900

    SWIFT

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
+    
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    
+    var diceArray :[UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        firstImageView.image = diceArray[1]
+        secondImageView.image = diceArray[1]
     }
 
-
+    
+    
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        //주사위 이미지 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        secondImageView.image = diceArray.randomElement()
+        
+         
+        
+        
+    }
+    
+    
 }
 
