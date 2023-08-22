a37e06f12a245a3df4ab5edf07b3b55ab60b47a0
kim_ingyeom <doctok501@gmail.com>
2023-08-22 15:46:06 +0900

    SWIFT

---
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by 김인겸 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
